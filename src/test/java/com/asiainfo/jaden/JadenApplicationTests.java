d40b478951e583a33684f3e7c1fb13b4bd1eef6e
JadenWa <jadenwa>
2023-12-18 17:05:57 +0800

    Initial commit

---
+++ src/test/java/com/asiainfo/jaden/JadenApplicationTests.java
@@ -0,0 +1,14 @@
+package com.asiainfo.jaden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JadenApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
